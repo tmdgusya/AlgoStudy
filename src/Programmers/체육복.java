1cf8d71f0c9f433eaf48431607acd3b7b045955f
tmdgusya <tmdgusya@naver.com>
2021-02-03 12:36:22 +0900

    [프로그래머스 체육복]

---
+++ src/Programmers/체육복.java
@@ -0,0 +1,57 @@
+package Programmers;
+
+import java.util.Arrays;
+
+public class 체육복 {
+
+    public static void main(String[] args) {
+        int[] lost = {2,4};
+        int[] reserve = {1,3,5};
+        int n = 5;
+        int expectedData = 5;
+        int result = Solution.solution(n, lost, reserve);
+        System.out.println("Result = " + result + " ExpectedData = " + expectedData);
+    }
+
+    static class Solution {
+        public static int solution(int n, int[] lost, int[] reserve) {
+            int answer = 0;
+            int[] clothes = new int[n];
+            for(int i = 0; i < n; i++){
+                clothes[i] = 1;
+            }
+            for(int idx : lost){
+                clothes[idx-1] -= 1;
+            }
+            for(int idx : reserve){
+                clothes[idx-1] += 1;
+            }
+            for(int i = 0; i < n; i++){
+                int frontIdx = i-1;
+                int backIdx = i+1;
+                if(clothes[i] == 2){
+                    if(frontIdx >= 0){
+                        if(clothes[frontIdx] == 0){
+                            clothes[frontIdx] += 1;
+                            clothes[i] -= 1;
+                        }
+                    }
+                }
+                if(clothes[i] == 2){
+                    if(backIdx < n){
+                        if(clothes[backIdx] == 0){
+                            clothes[backIdx] += 1;
+                            clothes[i] -= 1;
+                        }
+                    }
+                }
+            }
+            for(int i : clothes){
+                if(i > 0){
+                    answer++;
+                }
+            }
+            return answer;
+        }
+    }
+}

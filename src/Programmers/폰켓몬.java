91da966f35b18ac4d18ba07a2bd670c1ad2db3df
tmdgusya <tmdgusya@naver.com>
2021-02-15 18:44:49 +0900

    [프로그래머스 - 폰켓몬]

---
+++ src/Programmers/폰켓몬.java
@@ -0,0 +1,26 @@
+package Programmers;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class 폰켓몬 {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(new int[]{3,1,2,3}));
+    }
+
+    static class Solution {
+        public int solution(int[] nums) {
+            Set<Integer> set = new HashSet<>();
+            for (int a : nums){
+                set.add(a);
+            }
+            if(set.size() < nums.length/2) {
+                return set.size();
+            }else
+                return nums.length/2;
+        }
+    }
+}

626ad7425908a1dd11a4271f2552a7b9b1751b30
tmdgusya <tmdgusya@naver.com>
2021-03-23 21:47:17 +0900

    algo :question: Back10872

---
+++ src/BackjunJudge/Back10872.java
@@ -0,0 +1,25 @@
+package BackjunJudge;
+
+import java.io.*;
+
+public class Back10872 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int answer = factorial(n);
+
+        System.out.println(answer);
+
+    }
+
+    public static int factorial(int n) {
+        if(n<=1) {
+            return 1;
+        }
+        return n*factorial(n-1);
+    }
+
+}
